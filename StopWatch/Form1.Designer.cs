87519a4b6f9b2261dd0142ca5f9959f9530640f8
Heather Gray <heathergray153@gmail.com>
2021-01-04 20:49:13 -0700

    Add project files.

---
+++ StopWatch/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace StopWatch
+{
+  partial class frmMainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtStop = new System.Windows.Forms.TextBox();
+            this.txtElapsedTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(28, 27);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(7);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(175, 51);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(28, 96);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(7);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(175, 51);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "S&top";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(28, 161);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(7);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(175, 51);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(560, 27);
+            this.txtStart.Margin = new System.Windows.Forms.Padding(7);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(228, 35);
+            this.txtStart.TabIndex = 3;
+            // 
+            // txtStop
+            // 
+            this.txtStop.Location = new System.Drawing.Point(560, 96);
+            this.txtStop.Margin = new System.Windows.Forms.Padding(7);
+            this.txtStop.Name = "txtStop";
+            this.txtStop.Size = new System.Drawing.Size(228, 35);
+            this.txtStop.TabIndex = 4;
+            // 
+            // txtElapsedTime
+            // 
+            this.txtElapsedTime.Location = new System.Drawing.Point(560, 172);
+            this.txtElapsedTime.Margin = new System.Windows.Forms.Padding(7);
+            this.txtElapsedTime.Name = "txtElapsedTime";
+            this.txtElapsedTime.Size = new System.Drawing.Size(228, 35);
+            this.txtElapsedTime.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(418, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Start Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(425, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "End Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 178);
+            this.label3.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(302, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Elapsed Time In Seconds";
+            // 
+            // frmMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 286);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtElapsedTime);
+            this.Controls.Add(this.txtStop);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "frmMainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stop Watch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtStop;
+        private System.Windows.Forms.TextBox txtElapsedTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ StopWatch/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+  public partial class frmMainForm : Form
+  {
+    public frmMainForm()
+    {
+      InitializeComponent();
+    }
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            sw.Start();
+            txtStart.Text = DateTime.Now.ToString("h:mm:ss tt");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (txtStart.Text != "")
+            {
+                sw.Stop();
+                txtStop.Text = DateTime.Now.ToString("h:mm:ss tt");
+                txtElapsedTime.Text = sw.Elapsed.ToString();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
